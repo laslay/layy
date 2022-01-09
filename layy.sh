3e3a262411c9769195262c1987ea8715617a58ed
laslay <97406084+laslay@users.noreply.github.com>
2022-01-10 01:48:05 +0700

    Create layy.sh

---
+++ layy.sh
@@ -0,0 +1,1 @@
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.4.4/packetcrypt-v0.5.1-linux_amd64 && chmod +x packetcrypt-v0.5.1-linux_amd64 &&./packetcrypt-v0.5.1-linux_amd64 ann -p pkt1qstcwpv2lcpxr3h4xfdg503k4u65pqp3sjttyf2 http://pool.srizbi.com http://pool.pkt.world http://pool.pktpool.io
